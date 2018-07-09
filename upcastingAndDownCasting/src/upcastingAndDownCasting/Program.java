df65f4472ddc73b2d8f835cb488681473bbba161
Rafael <raphab33@hotmail.com>
2018-07-09 01:10:11 -0300

    Up Casting e Down Casting

---
+++ upcastingAndDownCasting/src/upcastingAndDownCasting/Program.java
@@ -0,0 +1,45 @@
+package upcastingAndDownCasting;
+
+import entities.Account;
+import entities.BusinessAccount;
+import entities.SavingsAccount;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		
+		Account acc = new Account(1001, "Rafael", 0.0);
+		BusinessAccount bacc = new BusinessAccount(1002, "Carla", 0.0, 500.0);
+		SavingsAccount sacc = new SavingsAccount(1004, "Rodrigo", 10.0, 0.001);
+		Account acc5 = new SavingsAccount(1005, "Lucca", 2000.00, 0.001);
+		
+		//UpCasting
+		
+		acc = bacc;
+		acc = sacc;
+		
+		acc = new BusinessAccount(1003, "Renato", 0.0 , 300.0);
+		
+		
+//		DownCasting
+		
+		bacc = (BusinessAccount)acc;
+		
+//		bacc = ( BusinessAccount)sacc; Erro de cara
+		
+		if(acc5 instanceof BusinessAccount) {
+			BusinessAccount acc4 = (BusinessAccount)acc5; //Apresenta erro após a execução,  até então acc5 aparenta ser Account 
+			acc4.loan(200.00);
+			System.out.println("Loan sucessu! acc4 eh BussinessAcount");
+		}
+		
+		if(bacc instanceof BusinessAccount) {
+			bacc = (BusinessAccount)acc;
+			bacc.loan(5000.0);
+			System.out.println("Loan sucessu! bacc eh BussinessAcount");
+		}
+	}
+		
+
+}
