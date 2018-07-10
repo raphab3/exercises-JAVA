faa78afd3aafc867eb962431980449b05bca91ac
Rafael <raphab33@hotmail.com>
2018-07-10 04:18:30 -0300

    Arquivos - FileWriter e BufferedWriter

--- arquivos/src/application/program.java
+++ arquivos/src/application/program.java
@@ -1,29 +1,28 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class program {
 
 	public static void main(String[] args) {
 	
 		
-		String path = "C:\\workspace\\wp-eclipse\\arquivos\\in.txt";
+		String[] lines = new String[] {"rafael" ,"carla" ,"lucca"};
 		
+		String path = "C:\\workspace\\wp-eclipse\\arquivos\\out.txt";
 		
-		try (BufferedReader br = new BufferedReader(new FileReader(path))){
-				String line = br.readLine();
-				
-				while(line != null) {
-					System.out.println(line);
-					line = br.readLine();
-				}
-			}catch(IOException e) {
-				System.out.println("Erro :" + e.getMessage());
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
+			for(String line : lines) {
+				bw.write(line);
+				bw.newLine();
+			}
 		}
+		catch(IOException ioe) {
+			ioe.printStackTrace();
+		}
+		
 			
 		
 	}
