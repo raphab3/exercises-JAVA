41bf90f712e3d8790e0d23eb35d07762eb2a8a33
Rafael <raphab33@hotmail.com>
2018-07-10 01:03:53 -0300

    Exercício de exeções (Ruim) Delegação para Class Responsavel porém retorna String

--- tratamentoDeExecoes1/src/application/Program.java
+++ tratamentoDeExecoes1/src/application/Program.java
@@ -28,8 +28,6 @@ public class Program {
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-		
-
 		System.out.print("Room number: ");
 
 		int number = sc.nextInt();
@@ -42,7 +40,6 @@ public class Program {
 
 		Date checkOut = sdf.parse(sc.next());
 
-		
 
 		if (!checkOut.after(checkIn)) {
 
@@ -69,34 +66,15 @@ public class Program {
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 
 			checkOut = sdf.parse(sc.next());
-
 			
-
-			Date now = new Date();
-
-			if (checkIn.before(now) || checkOut.before(now)) {
-
-				System.out.println("Error in reservation: Reservation dates for update must be future dates");
-
-			}
-
-			else if (!checkOut.after(checkIn)) {
-
-				System.out.println("Error in reservation: Check-out date must be after check-in date");
-
-			}
-
-			else {
-
-				reservation.updateDates(checkIn, checkOut);
-
+			String error = reservation.updateDates(checkIn, checkOut);
+			if(error != null) {
+				System.out.println("Error in Reservation: " + error);
+			}else {
 				System.out.println("Reservation: " + reservation);
-
 			}
 
 		}
-
-
 
 		sc.close();
 
