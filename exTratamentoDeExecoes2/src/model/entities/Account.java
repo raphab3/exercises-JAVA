8f8a4a9ba91fd5f300d578327405159f797e63c6
Rafael <raphab33@hotmail.com>
2018-07-10 02:38:38 -0300

    Exercício de exeções 2

---
+++ exTratamentoDeExecoes2/src/model/entities/Account.java
@@ -0,0 +1,67 @@
+package model.entities;
+
+import model.exception.DomainException;
+
+public class Account {
+	private Integer number;
+	private String holder;
+	private Double balance;
+	private Double withdrawLimit;
+	
+	public Account() {
+	}
+
+	public Account(Integer number, String holder, Double balance, Double withdrawLimit) throws DomainException {
+		if(balance < 0) {
+			throw new DomainException("Can not add negative value to account");
+		}
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+		this.withdrawLimit = withdrawLimit;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public Double getWithdrawLimit() {
+		return withdrawLimit;
+	}
+
+	public void deposit(Double amount) {
+		balance += amount;
+	}
+	
+	public void withdraw(Double amount) throws DomainException{
+		if(amount > balance ) {
+			throw new DomainException("Withdraw error: Not enough balance");
+		}
+		if(amount > withdrawLimit) {
+			throw new DomainException("Withdraw error: The amount exceeds withdraw limit");
+		}
+		else {
+			balance -= amount;
+		}
+		
+	}
+	
+	
+	
+}
