a9762bc6aa87aaa50e7085d711a609a59567bdef
Rafael <raphab33@hotmail.com>
2018-07-10 03:38:08 -0300

    Lendo arquivo texto com classes File e Scanner

---
+++ arquivos/src/application/program.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class program {
+
+	public static void main(String[] args) {
+	
+		
+		File file = new File("C:\\workspace\\wp-eclipse\\arquivos\\in.txt");
+		Scanner sc = null;
+		
+		try {
+			sc = new Scanner(file);
+			while(sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		}catch(IOException e) {
+			System.out.println("Erro :" + e.getMessage());
+		}finally {
+			if(sc != null) {
+				sc.close();
+			}
+			
+		}
+	}
+
+}
