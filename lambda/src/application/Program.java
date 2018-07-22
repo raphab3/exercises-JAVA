0e6c2d76aea744a96d8478e193c2606fb9014169
Rafael <raphab33@hotmail.com>
2018-07-22 04:59:16 -0300

    Expressão Lambda com Function

--- lambda/src/application/Program.java
+++ lambda/src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
 import entities.Product;
 
@@ -18,11 +19,11 @@ public class Program {
 		list.add(new Product("Tablet", 350.50)); 
 		list.add(new Product("HD Case", 80.90));
 		
-		double tax = 1.5;
 		
-		list.forEach(p -> p.setPrice(p.getPrice() * tax));
 		
-		list.forEach(System.out::println);
+		List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
+		
+		names.forEach(System.out::println);
 
 	}
 
